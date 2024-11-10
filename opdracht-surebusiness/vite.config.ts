add5c31131dc3a8cded6dbe30216247a012dddc6
bhalial <jstengs@gmail.com>
2024-11-10 14:39:00 +0100

    vite working with vercel?

--- opdracht-surebusiness/vite.config.ts
+++ opdracht-surebusiness/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vercel()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Set the alias for `@` to the `src` directory
