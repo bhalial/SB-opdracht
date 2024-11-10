3cf29a8b5595687261a8a45764fc50068122215e
bhalial <jstengs@gmail.com>
2024-11-10 16:32:55 +0100

    button text color in pagination was still set to white

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -27,7 +27,7 @@ export default defineComponent({
         buttonClasses() {
             return {
                 'px-4 py-2': true,
-                'bg-blue-200 text-white hover:bg-blue-300 text-blue-950': this.variant === 'primary' && !this.disabled,
+                'bg-blue-200 text-black hover:bg-blue-300 text-blue-950': this.variant === 'primary' && !this.disabled,
                 'bg-gray-100 text-gray-300 cursor-not-allowed': this.disabled,
                 'bg-gray-200 text-black hover:bg-gray-300 text-gray-950': this.variant === 'secondary' && !this.disabled,
             };
